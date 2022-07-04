2b10e3c4c9e339c23bdec6490a358d0dd843a3b0
mrjorgexavier <jorge.xavier@amaro.com>
2022-07-04 10:44:06 -0300

    lgos

--- Pod/Classes/SEGAppboyIntegration.m
+++ Pod/Classes/SEGAppboyIntegration.m
@@ -69,14 +69,14 @@
                 inApplication:[UIApplication sharedApplication]
             withLaunchOptions:nil
             withAppboyOptions:mergedAppboyOptions];
-      NSLog([NSString stringWithFormat: @"[Appboy startWithApiKey:%@ , inApplication:withLaunchOptions:withAppboyOptions: %@]",appboyAPIKey, mergedAppboyOptions]);
+      NSLog([NSString stringWithFormat: @"[Braze] isMainThread [Appboy startWithApiKey:%@ , inApplication:withLaunchOptions:withAppboyOptions: %@]",appboyAPIKey, mergedAppboyOptions]);
     } else {
       dispatch_sync(dispatch_get_main_queue(), ^{
         [Appboy startWithApiKey:appboyAPIKey
                   inApplication:[UIApplication sharedApplication]
               withLaunchOptions:nil
               withAppboyOptions:mergedAppboyOptions];
-        NSLog([NSString stringWithFormat: @"[Appboy startWithApiKey:%@ , inApplication:withLaunchOptions:withAppboyOptions: %@]",appboyAPIKey, mergedAppboyOptions]);
+        NSLog([NSString stringWithFormat: @"[Braze] [Appboy startWithApiKey:%@ , inApplication:withLaunchOptions:withAppboyOptions: %@]",appboyAPIKey, mergedAppboyOptions]);
       });
     }
 #if !TARGET_OS_TV
